1df3b60cc113f9bbfbb4c3938eece969bcf7a37e
Bogdan Rosca <bogdan.rosca.drago@gmail.com>
2026-01-31 11:33:59 +0200

    Remove example from repo (ignored now)

--- example/lerobot_101_6_salut/build/lerobot_101_6_salut_description/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/mnt/hgfs/sharedprj/test_export/lerobot_101_6_salut/install/lerobot_101_6_salut_description'
